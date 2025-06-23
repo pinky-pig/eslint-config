8808648dc0b7fd3108c8067203ac35ac32c4c5f7
pinky-pig <2545589993@qq.com>
2025-06-23 14:06:28 +0800

    updated

--- src/configs/unocss.ts
+++ src/configs/unocss.ts
@@ -1,11 +1,19 @@
 import type { Config } from '../types'
 
 export const unocss = async (): Promise<Config[]> => {
-  const { default: unocss } = await import('@unocss/eslint-plugin')
-  return [
-    {
-      ...(unocss.configs.flat as any as Config),
-      name: 'arvinn/unocss',
-    },
-  ]
+  try {
+    const { default: unocss } = await import('@unocss/eslint-plugin')
+    return [
+      {
+        ...(unocss.configs.flat as any as Config),
+        name: 'arvinn/unocss',
+      },
+    ]
+  } catch (error) {
+    // 如果导入失败或没有找到配置文件，返回空数组
+    console.warn(
+      '[@arvinn/eslint-config] UnoCSS plugin failed to load, skipping',
+    )
+    return []
+  }
 }
